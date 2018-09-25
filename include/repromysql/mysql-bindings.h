7b036a8c3a973b9121c5b06baa04a4c6aa67d8c2
mike <mike@oha7.org>
2018-09-25 23:37:35 +0200

    my_bool as char

--- include/repromysql/mysql-bindings.h
+++ include/repromysql/mysql-bindings.h
@@ -86,8 +86,8 @@ protected:
 	enum_field_types	type_;
 	std::shared_ptr<char> buf_;
 	long unsigned int	maxlen_;
-	char				is_null_;
-	char				is_err_;
+	my_bool				is_null_;
+	my_bool				is_err_;
 
 	union
 	{
